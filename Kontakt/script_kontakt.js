701bf791bd087812741e22104c3aa49fc7a7a788
vanjamodrinjak21 <adynamics21@gmail.com>
2022-12-05 09:57:46 +0100

    proba

--- Kontakt/script_kontakt.js
+++ Kontakt/script_kontakt.js
@@ -1,3 +1,13 @@
+
+  document.getElementById('menu').addEventListener('click', function () {
+    var nav = document.getElementsByTagName('nav')[0];
+    if (nav.style.display == 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+  }, false);
+
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
     e.target.elements.name.value = '';
